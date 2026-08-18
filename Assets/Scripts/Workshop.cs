8670e48640445577f07addf78045b0d5e8a416e8
patbadin-wq <patbadin54485448@gmail.com>
2026-08-18 16:22:11 +0700

    Patbadin Week01 เสร็จละ

--- Assets/Scripts/Workshop.cs
+++ Assets/Scripts/Workshop.cs
@@ -12,45 +12,114 @@ public class Workshop : MonoBehaviour
         // As04_AndOrOperatorExample();
         // As05_GuessingNumberExample();
         // As06_GuessingNumberMoreOrLessExample();
-        // As07_VerifyIdentityExample();
+         As07_VerifyIdentityExample();
     }
 
     public bool isSixOClock;
     public void As01_SyntaxIf()
     {
-        
+        if (isSixOClock)
+        {
+            Debug.Log("The door open");
+        }
+        Debug.Log("Knock Knock");
     }
 
     public string password;
     public void As02_StringComparisonExample()
     {
-        
+        if (password == "Moon") 
+        {
+            Debug.Log("Password is correct");
+        }
+
+        if (password != "Moon") 
+        {
+            Debug.Log("Password is incorrect");
+        }
     }
 
     public int as03Number;
     public void As03_NumberComparisonExample()
     {
+        if (as03Number > 10) 
+        {
+            Debug.Log("My number > 10");
+        }
 
+        if (as03Number >= 10) 
+        {
+            Debug.Log("My number >= 10");
+        }
+
+        if (as03Number < 10)
+        {
+            Debug.Log("My number < 10");
+        }
+
+        if (as03Number <= 10)
+        {
+            Debug.Log("My number <= 10");
+        }
+
+        if (as03Number == 10)
+        {
+            Debug.Log("My number == 10");
+        }
+
+        if (as03Number != 10)
+        {
+            Debug.Log("My number != 10");
+        }
     }
 
     public int as04Number;
     public void As04_AndOrOperatorExample()
     {
+        if (as04Number > 8 && as04Number < 12) 
+        {
+            Debug.Log("My Number 8> <12");
+        }
 
+        if (as04Number < 8 || as04Number > 12) 
+        {
+            Debug.Log("My number 8 || 12");
+        }
     }
 
     public int as05GuessingNumber;
     public int as05RandomNumber;
     public void As05_GuessingNumberExample()
     {
+        if (as05GuessingNumber == as05RandomNumber) 
+        {
+            Debug.Log("Congratulation");
+        }
 
+        else 
+        {
+            Debug.Log("Try again");
+        }
     }
 
     public int as06GuessingNumber;
     public int as06RandomNumber;
     public void As06_GuessingNumberMoreOrLessExample()
     {
+        if (as06GuessingNumber < as06RandomNumber) 
+        {
+            Debug.Log("Too Low");
+        }
 
+        else if (as06GuessingNumber > as06RandomNumber)
+        {
+            Debug.Log("Too High");
+        }
+
+        else 
+        {
+            Debug.Log("Congratulation");
+        }
     }
 
     public string as07Username;
@@ -59,6 +128,27 @@ public class Workshop : MonoBehaviour
     public bool as07IsPaid;
     public void As07_VerifyIdentityExample()
     {
+        if (as07Username == "user" && as07Password == "user67") 
+        {
+            Debug.Log("You have accessed");
+            if (as07IsPaid)
+            {
+                Debug.Log("VIP member");
+            }
+            else 
+            {
+                Debug.Log("Free member");
+            }
 
+            if (as07Age >= 18) 
+            {
+                Debug.Log("You have access to exclusive content");
+            }
+        }
+
+        else 
+        {
+            Debug.Log("You are guest");
+        }
     }
 }
